7fa410077bef668ed0fb06db74b3f163210ac558
Trevor Ramey <trevor.ramey@ttu.edu>
2019-10-11 15:57:42 -0500

    Add AppProvider and hook up to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AppContext } from "./AppProvider";
 
 const Logo = styled.div`
   font-size: 1.5em;
@@ -14,18 +15,29 @@ const Bar = styled.div`
 const ControlButtonElem = styled.div`
   cursor: pointer;
   ${props =>
-    props.active && 
+    props.active &&
     css`
-      text-shadow: 0px 0px 60px #03ff03;
+      text-shadow: 0px 0px 30px #03ff03;
     `}
 `;
 
 function toProperCase(lower) {
-    return lower.charAt(0).toUpperCase() + lower.substr(1);
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
 function ControlButton({ name, active }) {
-  return <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>;
+  return (
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem 
+        active={page === name}
+        onClick={()=> setPage(name)}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
 }
 
 export default function() {
--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import WelcomeMessage from './WelcomeMessage'
-import AppLayout from './AppLayout'
-import AppBar from './AppBar';
-
+import React from "react";
+import "./App.css";
+import WelcomeMessage from "./WelcomeMessage";
+import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
+import {AppProvider} from "./AppProvider";
 
 function App() {
   return (
     <AppLayout>
-      <AppBar/>
-      <WelcomeMessage/>
-      
+      <AppProvider>
+        <AppBar />
+        <WelcomeMessage />
+      </AppProvider>
     </AppLayout>
-    
   );
 }
 
