f7800860321f1e1e0f144890302adff2bb66a713
Trevor Ramey <treramey@gmail.com>
2019-10-13 16:15:25 -0500

    Settings page, localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React from "react";
 
 export const AppContext = React.createContext();
 
+export  class AppProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "dashboard",
+      ...this.savedSettings(),
+      setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites
+    };
+  }
 
-export class AppProvider extends React.Component {
-    constructor(props){
-        super(props);
-        this.state ={
-          page:'Settings' ,
-          setPage: this.setPage
-        }
-    }
-    setPage = page => this.setState({page})
+  confirmFavorites= () => {
+    this.setState({
+       firstVisit: false,
+       page: 'dashboard'
+    });
+    localStorage.setItem('cryptoDash', JSON.stringify({
+        test: 'hello'
+    }))
+  }
 
-    render(){
-        return(
-            <AppContext.Provider value={this.state}>
-                {this.props.children}
-            </AppContext.Provider>
-        )
+  savedSettings() {
+    let cryptoDashData = JSON.parse(localStorage.getItem("cryptoDash"));
+    if (!cryptoDashData) {
+      return { page: "settings", firstVisit: true };
     }
+    return {};
+  }
+
+  setPage = page => this.setState({ page });
+
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
 }
