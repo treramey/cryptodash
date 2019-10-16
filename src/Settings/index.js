d3e25731ddca27838d76ca590e85f4ceb1fe4f33
Trevor Ramey <trevor.ramey@ttu.edu>
2019-10-16 16:27:53 -0500

    Add Search

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,6 +3,7 @@ import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from './ConfirmButton'
 import Page from '../Shared/page';
 import CoinGrid from './Coingrid';
+import Search from './Search';
 
 export default function() {
   return (
@@ -10,6 +11,7 @@ export default function() {
       <WelcomeMessage /> 
       <CoinGrid topSection/>
       <ConfirmButton />
+      <Search/>
       <CoinGrid/>
     </Page>
   );
